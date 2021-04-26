ce1c941314d1996fe68c0266f17cdf54e00b7de4
Sebman-GH <82310686+Sebman-GH@users.noreply.github.com>
2021-04-26 18:52:27 +0300

    Version 2. Now client can transfer files.
    -Fixed bugs with /disconnect comand
    -Added new option to send file
    -Added comand list in main menu
    -Fixed minor bugs

--- client.py
+++ client.py
@@ -1,54 +1,90 @@
 import socket
+import os
+from time import sleep
 
 HEADER = 16
 PORT = 6067
 FORMAT = 'utf-8'
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def main_menu(fst_start):
-    if fst_start == True:
-        
-        SERVER = input("Input the server IP you would like to connect:")
-        ADDR = (SERVER,PORT)
-        print("Connecting...")
-        try:
-            client.connect(ADDR)
-            print("Connected to the server.")
-        except:
-            print("Failed connect to the server!")
-            main_menu(True)
-    chs_temp = int(input("Choose the option: \n 1.Send the message \n 2.Send the file(unavaiable now) \n "))
-    if chs_temp == 1: send_message()
-    elif chs_temp == 2: send_file()
+def main_menu(*fst_start):
+    try:
+        if fst_start[0] == True:
+            SERVER = '192.168.56.1'
+            #SERVER = input("Input the server IP you would like to connect:")
+            ADDR = (SERVER,PORT)
+            print("Connecting...")
+            try:
+                client.connect(ADDR)
+                print("Connected to the server.")
+                sleep(0.5)
+            except:
+                print("Failed connect to the server!")
+                sleep(1)
+                main_menu(True)
+    except IndexError:
+        pass
+    chs_temp = input("Choose the option: \n 1.Send the message \n 2.Send the file \n 3.All commands in the program (/help) \n")
+    if chs_temp == "/disconnect": send_message("/disconnect")
+    elif chs_temp == '1': send_message()
+    elif chs_temp == '2': send_file()
+    elif chs_temp == '3': help_list()
     else: 
         print("Wrong input!")
-        main_menu(False)
+        main_menu()
 
 def send_message(*msg):
-    if msg == (): 
-        msg = input("Enter what you would like to send:")
-    if msg == "/return": main_menu(False)
-    try:
-        message = msg.encode(FORMAT)
-        send_lenth = str(len(message)).encode(FORMAT)
-        send_lenth += b' ' * (HEADER - len(send_lenth))
-        client.send(send_lenth)
-        client.send(message)
-    except:
-        print("Failed to send message")
-        send_message()
-    if msg != "/disconnect": 
+    while True:
+        if msg == (): msg = input("Enter what you would like to send:")
+        if msg == "/return": main_menu()
+        if type(msg) == tuple: msg = msg[0]
+        try:
+            client.send(b"1")
+            message = msg.encode(FORMAT)
+            client.send(lenth_sending(message))
+            client.send(message)
             print("Message sent successfull")
-            send_message()
-    else: 
-        quit()
+        except: 
+            print("Failed to send message")
+            main_menu(True)
+        if msg == "/disconnect": 
+            quit()
+        msg = ()
 
 def send_file():
-    print("Not realised now")
-    input()
-    main_menu(False)
-if __name__ == "__main__":
-    print("Sender v1")
-    main_menu(True)
-    send_message("/disconnect")
-socket.close
+    os.system("dir")
+    filename = input("Enter file name what you would like to send(in same directory): ")
+    if filename == "/disconnect": send_message("/disconnect")
+    if filename == "/return": main_menu()
+    try:
+        with open(filename,'rb') as file:
+            data = file.read()
+            filename = filename.encode(FORMAT)
+            client.send(b"2")
+            client.send(lenth_sending(filename))
+            client.send(filename)
+            client.send(lenth_sending(data))
+            client.sendall(data)
+            if input("Open file after sending? (y/n)\n") == 'y': client.send(b'y')
+            else: client.send(b'n')
+            print("File sent")
+            file.close()
+    except:
+        print("Failed to send file.")
+    main_menu()
+
+def lenth_sending(string):
+        string_lenth = str(len(string)).encode(FORMAT)
+        string_lenth += b' ' * (HEADER - len(string_lenth))
+        return string_lenth
+
+def cmd_input():
+    pass
+
+def help_list():
+    print('/return - returning to the main menu \n/disconnect - disconnecting from server and close program from any menu')
+    os.system("pause")
+    main_menu()
+
+print("Sender v2 (updated 04.26.2021)")
+main_menu(True)
--- server.py
+++ server.py
@@ -7,34 +7,56 @@ PORT = 6067
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER,PORT)
 FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "/disconnect"
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(ADDR)
 
 def handle_client(conn, addr):
-    DISCONNECT_MESSAGE = "/disconnect"
+
     print(f"NEW CONNECTION FROM: {addr}")
-    connected = True
-    while connected == True:
+    while True:
         try:
-            msg_lenth = conn.recv(HEADER).decode(FORMAT)
-            if msg_lenth:
-                msg_lenth = int(msg_lenth)
-                msg = conn.recv(msg_lenth).decode(FORMAT)
-                if msg == DISCONNECT_MESSAGE:
-                    connected = False
-                    print(f"User {addr} has disconnected.Active connections: { threading.activeCount() - 2 }")
-                else: 
-                    print(f"[{addr}]: {msg}")
+            chs_temp = conn.recv(1)
+            if chs_temp == b"1": msg_recv(conn,addr)
+            if chs_temp == b"2": file_recv(conn,addr)
         except:
-            connected = False
-            print(f"User {addr} has disconnected.Active connections: { threading.activeCount() - 2 }")
+            print(f"User {addr} has disconnected.Active connections {threading.activeCount() - 2}")
+            break
+
+def msg_recv(conn,addr):
+
+    msg_lenth = conn.recv(HEADER).decode(FORMAT)
+    if msg_lenth:
+        msg_lenth = int(msg_lenth)
+        msg = conn.recv(msg_lenth).decode(FORMAT)
+        if msg == DISCONNECT_MESSAGE:
+            print(f"User {addr} has disconnected.Active connections: {threading.activeCount() - 2}")
+        else: 
+            print(f"[{addr}]: {msg}")
+
+def file_recv(conn,addr):
+    try:
+        filename_lenth = int(conn.recv(HEADER).decode(FORMAT))
+        filename = str(conn.recv(filename_lenth).decode(FORMAT))
+        data_lenth = int(conn.recv(HEADER).decode(FORMAT))
+        data = conn.recv(data_lenth)
+        with open(filename, 'wb+') as file:
+            file.write(data)
+            file.close
+        print(f"New file {filename} received!")
+        if conn.recv(1) == b'y':
+            os.system(f"start {filename}")
+        else:
+            pass
+    except:
+        print(f"New file {filename} not received.")
 
 def start():
     server.listen()
     print(f"Server started on {SERVER}")
     while True:
         conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr,))
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
         print(f"Active connections: { threading.activeCount() - 1 } ")
 
